ec83651d5c573fcd7e4aea263f0ce49084b91507
oselin <pierfrancescooselin@gmail.com>
2021-04-30 14:45:55 +0200

    improving CarTrailer.h testing

--- tests/testCarTrailer.cpp
+++ tests/testCarTrailer.cpp
@@ -11,9 +11,6 @@ TEST_CASE("init should succeed with non zero value", "[oselin_init]") {
     OselinDevice *dev;
 
     REQUIRE(oselin_init(dev, d) == 1);
- 
-}
-
 
 TEST_CASE("oselin init copy should not return null value", "[oselin_init_acopyof]") {
     REQUIRE(oselin_init_acopyof(NULL) != NULL);
@@ -27,6 +24,33 @@ TEST_CASE("oselin_parsing should not add to dev->svg if svg is empty", "[oselin_
    
 }
 
-TEST_CASE("my_div should give fraction as double", "[fraction]") {
-    
+TEST_CASE("", "[oselin_to_svg]") {
+    void oselin_to_svg(OselinDevice *, bool = true, bool = false);
 }
+
+TEST_CASE("", "[oselin_trigonometry]") {
+    
+    void oselin_trigonometry(OselinDevice *, bool = true);
+}
+
+
+
+TEST_CASE("", "[oselin_parsing]") {
+    void oselin_parsing(OselinDevice *, std::string);
+}
+
+TEST_CASE("", "[errors]") {
+    std::string errors(int);
+}
+
+TEST_CASE("", "[oselin_set]") {
+    
+    OselinDevice *oselin_set(OselinDevice *, int, float);
+}
+
+TEST_CASE("", "[oselin_init_acopyof]") {
+    OselinDevice *oselin_init_acopyof(OselinDevice *);
+}
+
+
+
