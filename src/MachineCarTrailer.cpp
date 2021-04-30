ba63ab29a6ce0b9c5abe00a2922328452812ac01
oselin <pierfrancescooselin@gmail.com>
2021-04-30 14:02:32 +0200

    Updating code for @GiacomoCorradini 's new library version

--- src/MachineCarTrailer.cpp
+++ src/MachineCarTrailer.cpp
@@ -15,141 +15,33 @@ using namespace std;
 
 
 //IMPLEMENTING @GIACOMOCORRADINI CAR.H LIBRARY
-
-/**
- * Redefining svg conversion for car's wheels because the author did not implemented y-coordiante changes
- * @param coca_device (=car)
- **/
-string new_coca_strg_ruote(coca_device* macch){
-    string ruo;
-    // Ruota sinistra
-    ruo = "\t<circle ";
-    ruo += "cx=\"" + to_string(macch->sx.centrox) + "\" cy=\"" + to_string(macch->sx.centroy) + "\" ";
-    ruo += "r=\"" + to_string(macch->sx.ruota) + "\" stroke=\"black\" stroke-width=\"3\" fill=\"black\"/>\n\n";
-    // Cerchione sinistra
-    ruo += "\t<circle ";
-    ruo += "cx=\"" + to_string(macch->sx.centrox) + "\" cy=\"" + to_string(macch->sx.centroy) + "\" ";
-    ruo += "r=\"" + to_string(macch->sx.cerchione) + "\" stroke=\"black\" stroke-width=\"3\" fill=\"gray\"/>\n\n";
-    // Ruota destra
-    ruo += "\t<circle ";
-    ruo += "cx=\"" + to_string(macch->dx.centrox) + "\" cy=\"" + to_string(macch->dx.centroy) + "\" ";
-    ruo += "r=\"" + to_string(macch->dx.ruota) + "\" stroke=\"black\" stroke-width=\"3\" fill=\"black\"/>\n\n";
-    // Cerchione destra
-    ruo += "\t<circle ";
-    ruo += "cx=\"" + to_string(macch->dx.centrox) + "\" cy=\"" + to_string(macch->dx.centroy) + "\" ";
-    ruo += "r=\"" + to_string(macch->dx.cerchione) + "\" stroke=\"black\" stroke-width=\"3\" fill=\"gray\"/>\n\n";
-
-    return ruo;
-}
-
-/**
- * Fill some parameters which weren't considered or modified by the author @GiacomoCorradini
- * @param coca_device (=car)
- * @param param from which newparam can be calculated
- **/
-void oselin_coca_implementation(coca_device * macch, float param[5]){
-    
-    //coca_try_carrozzeria(coca_device * macch)
-    macch->car.width = param[0];
-    macch->car.height = param[1];
-    macch->car.cx = param[3];
-    macch->car.cy = param[4] - (macch->car.height - 1) / 2;
-
-    int diametro = (int)param[2];
-    int x = 1;//(int)param[5];
-    
-    //coca_try_ruote(coca_device *macch)
-    switch (diametro)
-    {
-        case 16:
-            macch->sx.ruota = (macch->car.height - 1) / 2;
-            macch->sx.cerchione = macch->sx.ruota / 1.6;
-            macch->dx.ruota = (macch->car.height - 1) / 2;
-            macch->dx.cerchione = macch->dx.ruota / 1.6;
-            break;
-        case 17:
-            macch->sx.ruota = macch->car.height / 2;
-            macch->sx.cerchione = macch->sx.ruota / 1.6;
-            macch->dx.ruota = macch->car.height / 2;
-            macch->dx.cerchione = macch->dx.ruota / 1.6;
-            break;
-        case 18:
-            macch->sx.ruota = (macch->car.height + 1) / 2;
-            macch->sx.cerchione = macch->sx.ruota / 1.6;
-            macch->dx.ruota = (macch->car.height + 1) / 2;
-            macch->dx.cerchione = macch->dx.ruota / 1.6;
-            break;
-        
-        default:
-            break;
-    }
-
-    //coca_try_assetto(coca_device* macch)
-    switch (x){
-        case 1:
-            macch->sx.centrox = macch->car.cx + (macch->car.width / 5) - 5;
-            macch->dx.centrox = macch->car.cx + macch->car.width - (macch->car.width / 5) + 5;
-            macch->sx.centroy = macch->car.cy + macch->car.height;
-            macch->dx.centroy = macch->car.cy + macch->car.height;
-
-            break;
-        case 2:
-            macch->sx.centrox = macch->car.cx + (macch->car.width / 5);
-            macch->dx.centrox = macch->car.cx + macch->car.width - (macch->car.width / 5);
-            macch->sx.centroy = macch->car.cy + macch->car.height;
-            macch->dx.centroy = macch->car.cy + macch->car.height;
-            break;
-        case 3:
-            macch->sx.centrox = macch->car.cx + (macch->car.width / 5) +5;
-            macch->dx.centrox = macch->car.cx + macch->car.width - (macch->car.width / 5) - 5;
-            macch->sx.centroy = macch->car.cy + macch->car.height;
-            macch->dx.centroy = macch->car.cy + macch->car.height;
-            break;
-
-        default:
-            break;
-    }
-
-    coca_try_finestrini(macch);
-    coca_try_spoiler(macch);
-    coca_try_tetto(macch);
-}
-
-/**
- * Convert car to svg
- * @param coca_device (or car struct)
- **/
-string oselin_coca_to_svg(coca_device * macch){
-    
-    string svg;
-    svg += coca_strg_carrozzeria(macch);
-    svg += new_coca_strg_ruote(macch);
-    svg += coca_strg_tetto(macch);
-    svg += coca_strg_finestrini(macch);
-    svg += coca_strg_spoiler(macch);
-    return svg;
-}
-
 /**
  * Initializate car struct
  * @param float[] -> parameters for the design
  * @param newx, newy car's position
  **/
 coca_device * oselin_coca_init(float param[6], float newx, float newy){
-    coca_device * dev = new coca_device;
+    
 
-    float newparam [5];
-    for (int i=0;i<3;i++) newparam[i] = param[i];
-    newparam[3] = newx;
-    newparam[4] = newy;
+    parametri arrayparam;
+    arrayparam.inwidth  = param[0];
+    arrayparam.inheight = param[1];
+    arrayparam.indiam   = param[2];
+    arrayparam.inpx  = newx;
+    arrayparam.inpy  = newy - param[2];
+    arrayparam.inass    = 1;
 
-    oselin_coca_implementation(dev, newparam);
+    //(macch->car.height - 1) / 2;
+    coca_device * dev = coca_init_device(arrayparam);
+    coca_try_finestrini(dev);
+    coca_try_spoiler(dev);
+    coca_try_tetto(dev);
+    //oselin_coca_implementation(dev, newparam);
 
     return dev;
 }
 
-//MACHINE 
-
+//MACHINE
 /**
  * Initialize a machine
  * @param OselinDevice trailers can be copied from
@@ -218,7 +110,7 @@ string oselin_machine_to_string(OselinMachine *mach, bool with_header){
         for (int i=0;i<mach->parameters[4];i++){
             for (int j=0;j < (int)mach->parameters[3]*len; j++ ){
                 svg += "<!--COCADEVICECAR-->\n";
-                svg += oselin_coca_to_svg(mach->cararray[i*(int)mach->parameters[3]*len + j]);
+                svg += coca_strg_device(mach->cararray[i*(int)mach->parameters[3]*len + j],0,0);
 
             }
         }
