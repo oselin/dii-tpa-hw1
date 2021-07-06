c34f002358c5929c17bbe1edce03a45a70af9543
oselin <pierfrancescooselin@gmail.com>
2021-07-06 19:23:55 +0200

    Rewriting the project in OOP style

--- include/car_trailer.h
+++ include/car_trailer.h
@@ -4,126 +4,158 @@
 
 #define DOWNOFFSET 50
 #include <string>
+#include <unordered_map>
 
-struct Parameters{
-    float svgwidth;
-    float svgheight;
-    float radius;
-    float length;
-    float height;
-    int   ncars;
-    int   nfloors;
-    float margin;
+
+using std::unordered_map;
+using std::string;
+
+unordered_map<std::string,float> parameters;
+
+namespace oselin{
+    class Svg{
+        protected:
+            float x_;
+            float y_;
+            float stroke_;
+            string color_;
+            string strokecolor_;
+        public:
+            Svg(){}
+            //Svg(){}
+            //Deconstructor;
+            
+            void  x(float inp);
+            float x();
+
+            void  y(float inp);
+            float y();
+            
+            void  stroke(float inp);
+            float stroke();
+
+            void   color(string inp);
+            string color();
+
+            void   strokecolor(string inp);
+            string strokecolor();
+    };
+
+    class Wheel: public Svg{
+        private:
+            float radius_;
+            string innercolor_;
+        public:
+            Wheel(){}
+
+            void  radius(float r);
+            float radius();
+            //Deconstructor;
+
+    };
+
+    class Floor: public Svg{
+        private:
+            float width_;
+            float height_;
+        public:
+            void  width(float w);
+            float width();
+            void  height(float h);
+            float height();
+    };
+
+    class Joint: public Svg{
+        private:
+            float length_;
+            Floor body;
+            Floor head;
+        public:
+            void  length(float l);
+            float length();
+
+    };
+
+    class Axis{
+        private:
+            Floor body;
+            Wheel bottom;
+            Wheel top;
+            float angle_ = 20;
+            float point_;
+        public:
+            void  angle(float a);
+            float angle();
+            void  point(float p);
+            float point();
+    };
+
+    class Trailer{
+        private:
+            string svg;
+            float length;
+            float x_;
+            float y_;
+            float offset_;
+
+            Floor downfloor;
+            Floor upfloor;
+            Wheel front_wheel;
+            Wheel rear_wheel;
+            Joint rear_joint;
+            Joint front_joint;
+            Axis rear_axis;
+            Axis front_axis;
+
+        public:
+            Trailer(unordered_map<string,float> parameters, bool avoid_svg);
+
+    };
 };
 
-struct Oselin_Wheel{
 
-    float x;
-    float y;
-    float radius;
-    float stroke;
-    std::string strokecolor = "black";
-    std::string outercolor  = "black";
-    std::string innercolor  = "gray";
+void  oselin::Svg::x(float inp){this->x_ = inp;}
 
-};
+float oselin::Svg::x(){return this->x_;}
 
-struct Oselin_Floor{
-    float x;
-    float y;
-    float width;
-    float height;
-    float stroke;
-    std::string strokecolor  = "black";
-    std::string fillingcolor = "#990000";
-};
+void  oselin::Svg::y(float inp){this->y_ = inp;}
 
-struct Oselin_Joint{
-    
-    float length;
-    Oselin_Floor body;
-    Oselin_Wheel head;
-};
+float oselin::Svg::y(){return this->y_;}
 
-struct Oselin_Axis{
-    
-    Oselin_Floor body;
-    Oselin_Wheel bottomscrew;
-    Oselin_Wheel topscrew;
-    float angle = 20;
-    float rotationpoint[2];
-};
+void  oselin::Svg::stroke(float inp){this->stroke_ = inp;}
 
-struct OselinDevice{
+float oselin::Svg::stroke(){return this->stroke_;}
 
-    std::string svg;
-    Parameters param;
-    float abslength;
-    float absx;
-    float absy;
-    float offset;
-    Oselin_Floor downfloor;
-    Oselin_Floor upfloor;
-    Oselin_Wheel frontwheel;
-    Oselin_Wheel rearwheel;
-    Oselin_Joint rearjoint;
-    Oselin_Joint frontjoint;
-    Oselin_Axis rearaxis;
-    Oselin_Axis frontaxis;
-};
+void  oselin::Svg::color(string inp){this->color_ = inp;}
 
-/**
- * Convert the device into a string
- * @param OselinDevice to convert
- * @param bool for adding or not the header
- * @param bool to add or not measures
- **/
-int oselin_to_svg(OselinDevice *, bool = true, bool = false);
+string oselin::Svg::color(){return this->color_;}
 
-/**
- * Do all the math for OselinDevice struct
- * @param OselinDevice to be calculated
- * @param bool for set automatically the offset for centering the drawing
- **/
-int oselin_trigonometry(OselinDevice *, bool = true);
+void  oselin::Svg::strokecolor(string inp){this->strokecolor_ = inp;}
 
-/**
- * Check if all the data are solid
- * @param OselinDevice to be filled
- * @param float with data to check
- * @param bool for bypassing some check operations
- **/
-OselinDevice *oselin_init(Parameters, bool = false);
+string oselin::Svg::strokecolor(){return this->strokecolor_;}
 
-/**
- * Parse an svg imported as string and fill the device
- * @param string svg
- * @param device to be filled
- **/
-OselinDevice * oselin_parsing(std::string, int = 0);
+void  oselin::Wheel::radius(float r){this->radius_ = r;}
 
-/**
- * Function for handling errors. It returns a string with the error type. It is invoked by all the other functions
- * @param int error code
- **/
-std::string errors(int);
+float oselin::Wheel::radius(){return this->radius_;}
 
-/**
- * Allow changing in the device
- **/
-OselinDevice *oselin_set_radius(OselinDevice *, float);
-OselinDevice *oselin_set_length(OselinDevice *, float);
-OselinDevice *oselin_set_height(OselinDevice *, float);
-OselinDevice *oselin_set_ncars(OselinDevice *, int);
-OselinDevice *oselin_set_nfloors(OselinDevice *, int);
-OselinDevice *oselin_set_svgwidth(OselinDevice *, float);
-OselinDevice *oselin_set_svgheight(OselinDevice *, float);
+void  oselin::Floor::width(float w){this->width_ = w;}
 
-/**
- * Return device-like struct allowing several copies
- * @param device (=trailer already existing)
- **/
-OselinDevice *oselin_init_acopyof(OselinDevice *);
+float oselin::Floor::width(){return this->width_;}
 
+void  oselin::Floor::height(float h){this->height_ = h;}
+
+float oselin::Floor::height(){return this->height_;}
+
+void  oselin::Joint::length(float l){this->length_ = l;}
+
+float oselin::Joint::length(){return this->length_;}
+
+void  oselin::Axis::angle(float a){this->angle_ = a;}
+
+float oselin::Axis::angle(){return this->angle_;}
+
+void  oselin::Axis::point(float p){this->point_ = p;}
+
+float oselin::Axis::point(){return this->point_;}
 
 #endif //CAR_TRAILER_H
